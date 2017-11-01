e0e73915a024a6aa6066b3eef7173a35d9c9a7f5
Yngve Hestem <yjhestem@stud.ntnu.no>
2017-11-01 13:18:20 +0100

    Uploading

---
+++ cmd/updateDB/main_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "Assignment2/exchange"
+import "testing"
+
+var testdb exchange.Storage
+var normaldb exchange.Storage
+
+func setupNormalDatabase() {
+	normaldb = databaseCred(false)
+}
+func setupTestdatabase() {
+	testdb = databaseCred(true)
+}
+
+func Test_GetTodaysCurrency(t *testing.T) {
+	setupNormalDatabase()						// Test this part
+	setupTestdatabase() //?
+	testdb.Init()		//?
+	ok := getCurrencyFromExternalDatabase(testdb, "latest")
+	if !ok {
+		t.Error("Function getTodaysCurrency(..) failed.")
+		return
+	}
+	ok = getCurrencyFromExternalDatabase(testdb, "latest")		// Checks for some other parts of the function
+	if !ok {
+		t.Error("Function getTodaysCurrency(..) failed on second time.")
+		return
+	}
+
+	ok = testdb.ResetCurrency()
+	if !ok {
+		t.Error("Couldn't reset Currency-collection")
+		return
+	}
+}
